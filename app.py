ba689727ea69f1d43d1c758afb7308e87906df22
OzzyCodes <ajwaldro@gmail.com>
2021-07-05 20:48:30 -0400

    updated templates

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('pages/placeholder.home.html')
+
+    return render_template('index.html')
+    # return render_template('pages/placeholder.home.html')
 
 
 @app.route('/about')
